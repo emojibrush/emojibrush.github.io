902b7232510f9dc775278a0e7ba8919483bb3b4a
jayschaul <jay@jayschaul.com>
2014-10-06 17:47:03 -0400

    kill share and redo blur

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ var background = document.getElementById('blur-canvas');
 var rect = { left: offset.left - 15, right: offset.left + offset.width + 15, top: offset.top, bottom: offset.bottom + 15 }
 
 function setBackgroundBlurredImage(imageData) {
-  /*
   var ctx = canvas.getContext('2d');
   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   var ctx2 = background.getContext('2d');
   ctx2.putImageData(imageData, -offset.left, 0);
   stackBlurCanvasRGBA("blur-canvas", 0, 0, background.width, background.height, 50);
-  */
 }
 
 function takeScreenShot() {
